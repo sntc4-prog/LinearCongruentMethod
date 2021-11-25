f4f8011407db8edc4f5156b3076c0083fd528b9b
Egor <egorborisovzypo@gmail.com>
2021-11-25 21:51:42 +0600

    Refactoring. The task is solved

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import Entity.LinearCongruentialGenerator;
 
 public class Main {
     public static void main(String[] args) {
-        LinearCongruentialGenerator linearCongGen = new LinearCongruentialGenerator(100000, 21, 19, 10);
+        LinearCongruentialGenerator linearCongGen = new LinearCongruentialGenerator(100000, 31, 23, 10);
         System.out.println("=========================================================");
         System.out.println(linearCongGen);
         System.out.println("=========================================================");
